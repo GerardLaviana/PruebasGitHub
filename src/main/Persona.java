16bb6ad7a1fff52f17caec6b04c1e2fc4b73f335
GerardLaviana <gerardlaviana@gmail.com>
2022-10-25 09:41:30 +0200

    Getters Setters Rama Personas

--- src/main/Persona.java
+++ src/main/Persona.java
@@ -2,9 +2,17 @@ package main;
 
 public class Persona {
 
-	String nombre;
+	private String nombre;
 
 	public Persona(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	
